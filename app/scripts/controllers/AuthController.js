b3d89cba60126ceb9b97d53c44b15fcea39dc0a4
diegosepulveda <diegoti89@gmail.com>
2016-07-30 05:20:18 -0400

    comienzo de auth

--- app/scripts/controllers/AuthController.js
+++ app/scripts/controllers/AuthController.js
@@ -1,4 +1,7 @@
 'use strict';
-angular.module('workshopApp').controller('AuthController', [function () {
+angular.module('workshopApp').controller('AuthController', ['$scope','$firebaseAuth', function ($scope,$firebaseAuth) {
     
+	var objAutenticacion = $firebaseAuth();
+
+
 }]);
