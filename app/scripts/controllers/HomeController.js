e42d69d4a87a3ca3977af144d2fef245015decfc
diegosepulveda <diegoti89@gmail.com>
2016-07-30 03:48:24 -0400

    inicio quickstart

--- app/scripts/controllers/HomeController.js
+++ app/scripts/controllers/HomeController.js
@@ -1,4 +1,12 @@
 'use strict';
-angular.module('workshopApp').controller('HomeController', [function () {
-    
+angular.module('workshopApp').controller('HomeController', ['$firebaseObject','$scope', function ($firebaseObject,$scope) {
+    var ref = firebase.database().ref(); //Creo una referencia apuntando a una direccion en la base datos de firebase, en este caso estoy apuntando al root de la BD
+	
+    var objFirebase = $firebaseObject(ref); // Le paso la referencia adonde esta apunto para crear un objeto
+
+
+
+
+
+
 }]);
