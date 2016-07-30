90e702ec8ab9655bf8cf6a9d61dbd37bf2217266
diegosepulveda <diegoti89@gmail.com>
2016-07-30 04:14:33 -0400

    comienzo trabajo con array en quickstart

--- app/scripts/controllers/QuickstartController.js
+++ app/scripts/controllers/QuickstartController.js
@@ -1,15 +1,5 @@
 'use strict';
 angular.module('workshopApp').controller('QuickstartController', ['$scope','$firebaseArray',function ($scope,$firebaseArray) {
     
-    var ref = firebase.database().ref().child('quickstart').child('lista');
 
-    var listaFirebase = $firebaseArray(ref);
-
-    listaFirebase.$add({
-    	'nombre' : 'Elon Musk',
-    	'edad' : '45',
-    	'profesion' : 'inventor,fisico'
-    });
-
-    $scope.lista = listaFirebase;
 }]);
