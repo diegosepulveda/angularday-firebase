75d8ccc1eb6ef8827a75411be47c9559584fe144
diegosepulveda <diegoti89@gmail.com>
2016-07-30 04:17:23 -0400

    comienzo de timeline

--- app/scripts/controllers/TimelineController.js
+++ app/scripts/controllers/TimelineController.js
@@ -1,4 +1,15 @@
 'use strict';
-angular.module('workshopApp').controller('TimelineController', [function () {
+angular.module('workshopApp').controller('TimelineController', ['$scope','$firebaseArray',function ($scope,$firebaseArray) {
+	$scope.events = [{
+	    badgeClass: 'info',
+	    badgeIconClass: 'glyphicon-check',
+	    title: 'First heading',
+	    content: 'Some awesome content.'
+	  }, {
+	    badgeClass: 'warning',
+	    badgeIconClass: 'glyphicon-credit-card',
+	    title: 'Second heading',
+	    content: 'More awesome content.'
+	  }];
     
 }]);
