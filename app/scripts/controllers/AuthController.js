f1f6b00cc942a65de8aadd83f88a458c44cd789e
diegosepulveda <diegoti89@gmail.com>
2016-07-30 06:02:56 -0400

    auth listo

--- app/scripts/controllers/AuthController.js
+++ app/scripts/controllers/AuthController.js
@@ -1,7 +1,44 @@
 'use strict';
-angular.module('workshopApp').controller('AuthController', ['$scope','$firebaseAuth', function ($scope,$firebaseAuth) {
+angular.module('workshopApp').controller('AuthController', ['$scope','$firebaseAuth', '$firebaseObject' , function ($scope,$firebaseAuth,$firebaseObject) {
     
 	var objAutenticacion = $firebaseAuth();
+	
+	$scope.loginUsuario = function(usuario,pass)
+	{
+		objAutenticacion.$signInWithEmailAndPassword(usuario, pass).then(function(firebaseUser) {
+		  console.log("Logeado como:", firebaseUser.uid);
+		  console.log("Usuario:", firebaseUser);
 
+		}).catch(function(error) {
+		  console.error("Authentication failed:", error);
+		});
+	}
+
+
+
+	$scope.logOut = function()
+	{
+		objAutenticacion.$signOut();
+	}
+
+	$scope.crearUsuario = function(usuario,pass)
+	{
+		objAutenticacion.$createUserWithEmailAndPassword(usuario,pass).then(function(firebaseUser) {
+		    console.log("User " + firebaseUser.uid + " created successfully!");
+		  }).catch(function(error) {
+		    console.error("Error: ", error);
+		  });
+	}
+
+
+
+	objAutenticacion.$onAuthStateChanged(function(firebaseUser) {
+	  if (firebaseUser) {
+	    console.log("Logeado como:", firebaseUser);
+	    $scope.firebaseUser = firebaseUser;
+	  } else {
+	    console.log("Log out"+$scope.firebaseUser);
+	  }
+	});
 
 }]);
