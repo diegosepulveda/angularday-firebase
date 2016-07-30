27adfdac756313d4bca0cda0c7e190e77bb09c5e
diegosepulveda <diegoti89@gmail.com>
2016-07-30 03:51:51 -0400

    primer hola mundo

--- app/scripts/controllers/HomeController.js
+++ app/scripts/controllers/HomeController.js
@@ -5,8 +5,8 @@ angular.module('workshopApp').controller('HomeController', ['$firebaseObject','$
     var objFirebase = $firebaseObject(ref); // Le paso la referencia adonde esta apunto para crear un objeto
 
 
-
-
-
+    objFirebase.$value = 'Hola Mundo'; //Creo un primer valor de prueba
+    objFirebase.$save();
+    
 
 }]);
