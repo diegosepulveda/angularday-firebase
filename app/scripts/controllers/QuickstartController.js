047945f81e67b47c16ed3072cba26764543d6c9b
diegosepulveda <diegoti89@gmail.com>
2016-07-30 04:12:21 -0400

    trabajo con arreglos

--- app/scripts/controllers/QuickstartController.js
+++ app/scripts/controllers/QuickstartController.js
@@ -1,4 +1,15 @@
 'use strict';
-angular.module('workshopApp').controller('QuickstartController', [function () {
+angular.module('workshopApp').controller('QuickstartController', ['$scope','$firebaseArray',function ($scope,$firebaseArray) {
     
+    var ref = firebase.database().ref().child('quickstart').child('lista');
+
+    var listaFirebase = $firebaseArray(ref);
+
+    listaFirebase.$add({
+    	'nombre' : 'Elon Musk',
+    	'edad' : '45',
+    	'profesion' : 'inventor,fisico'
+    });
+
+    $scope.lista = listaFirebase;
 }]);
