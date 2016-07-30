df337c222b38853769c635ae5f0bda031ccd1895
diegosepulveda <diegoti89@gmail.com>
2016-07-30 03:02:54 -0400

    inicio de la app

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name workshopApp
+ * @description
+ * # workshopApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('workshopApp', []);
